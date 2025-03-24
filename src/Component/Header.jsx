e431d018f838d337f862f6db79c80b9196ffd8b5
mahima <mahima@23navgurukul.org>
2025-03-24 15:55:11 +0530

    header

---
+++ src/Component/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faSearch,
+    faBell,
+    faChevronDown
+  } from '@fortawesome/free-solid-svg-icons';
+
+
+function Header() {
+    return(
+        <div className="header-component">
+             <h1>Dashboard<FontAwesomeIcon icon={faSearch} className="search-icon" /></h1>
+             <div className="header-logo">
+              <a href="#"><FontAwesomeIcon icon={faBell} /></a>
+              <img src="./girl.jpg" alt="girl-img"/>
+              <h2>Martin nel</h2>
+              <a href="#"><FontAwesomeIcon icon={faChevronDown} /></a>
+             </div>
+        </div>
+    )
+}
+export default Header;
