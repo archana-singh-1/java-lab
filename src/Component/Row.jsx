ee4234608769b65582cf587e1d872655031f6cf5
Priyanka Mishra <priyanka.mishra@example.com>
2025-03-24 16:17:48 +0530

    first commit

---
+++ src/Component/Row.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Row.css'
+import Column from './Column'
+
+function Row  ({columns})  {
+
+  return (
+    <>
+
+    <div className='row-container'>
+      {columns.map((column, index)=>(
+        <Column key={index} logo={column.logo} authorImg={column.authorImg} progress={column.progress} course={column.course} authorName={column.authorName}/>
+      ))}
+      <Column />
+      <Column />
+      <Column/>
+      {/* <Column/> */}
+           
+    </div>
+    </>
+  )
+}
+
+export default Row
