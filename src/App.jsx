d77d8a6c8483cf8d94c93416506902d520ecd9d8
Priyanka Mishra <priyanka.mishra@example.com>
2025-03-24 16:53:27 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import Row from "./Component/Row";
 
 
 
-
 const rowData = [
-  // first row
+
   [{ logo: "https://preview.redd.it/some-people-may-think-that-the-book-in-granny-is-the-book-v0-7ndcpqgu02jb1.png?width=640&crop=smart&auto=webp&s=1572fb87ecce9fd169e50d51469d8d9e667f7ffc", authorImg: "https://www.w3schools.com/w3images/avatar2.png",  progress: "Completed", course:"Design & Histor", authorName:"William joe" } ],
 
-  // second row
   [{ logo: "https://cdn-icons-png.flaticon.com/512/888/888859.png", authorImg: "https://www.w3schools.com/w3images/avatar5.png", progress: "80% Complete", course:"Web Development" , authorName:"Thamas Alva Edision"}],
-
-  // third row
 
   [{ logo: "https://cdn-icons-png.flaticon.com/512/919/919851.png", authorImg: "https://www.w3schools.com/w3images/avatar1.png" , progress: "70% Complete", course: "Software Development", authorName:"William joe" }]
 
-
 ]
+
+
 function App(){
 
   return(
@@ -27,10 +24,6 @@ function App(){
         <Row key={index} columns={row}/>
       ))}
     </div>
-    {/* <Row/>
-    <Row/>
-    <Row/> */}
-
     </>
   )
 }
