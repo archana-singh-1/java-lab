3c10eeb76955e96f8d0e7c078650b6ff4c109f11
Priyanka Mishra <priyanka.mishra@example.com>
2025-03-26 17:25:03 +0530

    delete row css

--- src/Details/DetailsCourse.jsx
+++ src/Details/DetailsCourse.jsx
@@ -7,7 +7,6 @@ function DetailsCourse () {
     <>
     <div className='whole-details-page'>
         <div className='details-container'>
-            {/* <div className='details-course'> */}
             <div className="course-video">
                 <iframe
                     src="https://www.youtube.com/embed/your-video-id" title="YouTube Video" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
@@ -56,9 +55,6 @@ function DetailsCourse () {
         <div className='detains-second'>
             <div className='details-second-section'>
                 <img src='/detains.png'></img>
-                {/* <h1>Course Content</h1>
-                <p>Lecture (15) Totla (5.5hrs)</p>
-                <div className='second-section-main'></div> */}
             </div>
         </div>
     </div>
