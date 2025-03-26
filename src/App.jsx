1863b86a797bec807e242730936fe0624f07887b
Priyanka Mishra <priyanka.mishra@example.com>
2025-03-26 10:22:27 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import DetailsCourse from "./Details/DetailsCourse";
 
 
 const rowData = [
-  // first row
+  
   [{ logo: "https://preview.redd.it/some-people-may-think-that-the-book-in-granny-is-the-book-v0-7ndcpqgu02jb1.png?width=640&crop=smart&auto=webp&s=1572fb87ecce9fd169e50d51469d8d9e667f7ffc", authorImg: "https://www.w3schools.com/w3images/avatar2.png",  progress: "Completed", course:"Design & Histor", authorName:"William joe" } ],
 
-  // second row
+ 
   [{ logo: "https://cdn-icons-png.flaticon.com/512/888/888859.png", authorImg: "https://www.w3schools.com/w3images/avatar5.png", progress: "80% Complete", course:"Web Development" , authorName:"Thamas Alva Edision"}],
 
-  // third row
 
   [{ logo: "https://cdn-icons-png.flaticon.com/512/919/919851.png", authorImg: "https://www.w3schools.com/w3images/avatar1.png" , progress: "70% Complete", course: "Software Development", authorName:"William joe" }]
 
